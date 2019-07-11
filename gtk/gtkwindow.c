7b71fe36ac151e49344b5430ebf7f2e21a3869e5
Adrien Plazas <kekun.plazas@laposte.net>
2019-07-11 14:18:39 +0200

    window: Maximize resizable pseudo-dialogs on mobile
    This will make them look better on phone screens.

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -39,6 +39,7 @@
 #include "gtkcssiconthemevalueprivate.h"
 #include "gtkcssrgbavalueprivate.h"
 #include "gtkcssshadowsvalueprivate.h"
+#include "gtkdialog.h"
 #include "gtkkeyhash.h"
 #include "gtkmain.h"
 #include "gtkmnemonichash.h"
@@ -6262,6 +6263,7 @@ gtk_window_map (GtkWidget *widget)
   GdkWindow *gdk_window;
   GList *link;
   GdkDisplay *display;
+  gboolean is_dialog;
 
   if (!_gtk_widget_is_toplevel (widget))
     {
@@ -6286,7 +6288,10 @@ gtk_window_map (GtkWidget *widget)
 
   gdk_window = _gtk_widget_get_window (widget);
 
-  if (priv->maximize_initially)
+  /* Transient windows can be considered pseudo-dialogs. */
+  is_dialog = GTK_IS_DIALOG (widget) || !!gtk_window_get_transient_for (window);
+  if (priv->maximize_initially ||
+      (is_dialog && gtk_window_get_resizable (window) && _gtk_get_is_phone ()))
     gdk_window_maximize (gdk_window);
   else
     gdk_window_unmaximize (gdk_window);
